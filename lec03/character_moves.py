d83de047e6668ea70d259a1a61b26e5777e9b59b
Leejuwon <bbit456@naver.com>
2018-09-18 21:19:18 +0900

    drill05_1
    이동경로 함수 나열

--- lec03/character_moves.py
+++ lec03/character_moves.py
@@ -5,58 +5,7 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-x = 0
-y = 90
-coutn = 0
+# 여기를 채우세요.
 
-def draw(x,y):
-    clear_canvas_now()
-    grass.draw_now(400,30)
-    character.draw_now(x,y)
-    delay(0.01)
-    
-while(1):
-
-    while(x<800):
-        draw(x,y)
-        x=x+10
-
-    while(y<600):
-        draw(x,y)
-        y=y+10
-
-    while(0<x):
-        draw(x,y)
-        x=x-10
-
-    while(90<y):
-        draw(x,y)
-        y=y-10
-
-    x=400
-    y=90
-
-    while(y<290):
-        draw(x,y)
-        x=x+40
-        y=y+40
-
-    while(400<x):
-        draw(x,y)
-        x=x-40
-        y=y+40
-
-    while(290<y):
-        draw(x,y)
-        x=x-40
-        y=y-40
-
-    while(x<400):
-        draw(x,y)
-        x=x+40
-        y=y-40
-
-    x=0
-    y=90
-    
 close_canvas()
+
